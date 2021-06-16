2a66f1692aeb184a32132f798acb8619d0e4c89b
Javiistacks <javiann.marrero@icloud.com>
2021-06-16 15:59:02 -0500

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer(props) {
+  return (
+    <footer class="row">
+      {/* <div class="col-lg"> */}
+        <p id="footer">©️ 2021 | Designed & built by Javiann Marrero</p>
+      {/* </div> */}
+    </footer>
+  );
+}
+
+export default Footer;
