fbcf663ec1d6764738433ce95981b1e5f717162a
Javiistacks <javiann.marrero@icloud.com>
2021-06-18 21:13:25 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Jumbotron from "./components/Header/Header";
 import Table from "./components/Table/Table";
-// import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <div>
       <Jumbotron />
       <Table />
+      <Footer />
   
     </div>
   );
